9447f630cc26335942d9bf48af5dd00739c86d4b
michalkerley3 <michael.kerley.3@us.af.mil>
2024-11-13 11:52:28 -0600

    changed build

--- Michael/build.sh
+++ Michael/build.sh
@@ -1,5 +1,4 @@
 
-
-export LD_LIBRARY_PATH=/home/vboxuser/projects/tangLinuxTest/Michael/code-gen/build/libs:/home/vboxuser/projects/tangLinuxTest/Michael/code-gen/local_install/lib
 make
+export LD_LIBRARY_PATH=/home/vboxuser/projects/tangLinuxTest/Michael/code-gen/build/libs:/home/vboxuser/projects/tangLinuxTest/Michael/code-gen/local_install/lib
 
